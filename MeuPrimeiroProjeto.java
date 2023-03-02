ee823dfd4b2ccdb8fe8ae04b77549d9569766cb7
Fernanda Hallmann <111521249+FernandaHallmann@users.noreply.github.com>
2023-03-02 15:56:13 -0300

    Add files via upload

---
+++ MeuPrimeiroProjeto.java
@@ -0,0 +1,13 @@
+package exercícios;
+
+public class MeuPrimeiroProjeto {
+    
+    public static void main (String [] args) {
+        
+        if (10 > 20) {
+            System.out.println("Olá Mundo!");
+        } else {
+            System.out.println("Olá Unipar!");
+        }
+    }
+}
