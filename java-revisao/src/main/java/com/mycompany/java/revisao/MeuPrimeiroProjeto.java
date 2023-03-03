36172f360b481f3f21c894c8784be87acd5d517e
Fernanda Hallmann <fernanda.hallmann@gmail.com>
2023-03-03 14:58:16 -0300

    Adicionando arquivos pelo git

---
+++ java-revisao/src/main/java/com/mycompany/java/revisao/MeuPrimeiroProjeto.java
@@ -0,0 +1,18 @@
+package com.mycompany.java.revisao;
+
+/**
+ *
+ * @author Ferna
+ */
+
+public class MeuPrimeiroProjeto {
+    
+    public static void main (String [] args) {
+        
+        if (10 > 20) {
+            System.out.println("Olá Mundo!");
+        } else {
+            System.out.println("Olá Unipar!");
+        }
+    }
+}
---
+++ java-revisao/src/main/java/com/mycompany/java/revisao/aluno.java
@@ -0,0 +1,30 @@
+package com.mycompany.java.revisao;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Ferna
+ */
+public class aluno {
+    
+    public static void main (String [] args) {
+        Scanner ler = new Scanner(System.in);
+        
+        System.out.println("Informe seu nome: ");
+        String nome = ler.nextLine();
+        
+        System.out.println("Informe seu sexo: ");
+        char sexo = ler.next().charAt(0);
+        sexo = Character.toLowerCase(sexo);
+        
+        System.out.print("Seu nome é " + nome);
+        switch (sexo) {
+            case 'm' -> System.out.println(" e seu sexo é masculino");
+            case 'f' -> System.out.println(" e seu sexo é feminino");
+            case 'g' -> System.out.println(" e seu sexo é gamer");
+            case 'l' -> System.out.println(" e você é louco");
+            default -> System.out.println(" e você é programador");
+        }
+    }
+}
